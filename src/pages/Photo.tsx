bad5bc48cadc412621ab6049710667019755889c
whyanbu <whyan@bu.edu>
2025-12-08 23:18:04 +0800

    added arrow buttons in photo page

--- src/pages/Photo.tsx
+++ src/pages/Photo.tsx
@@ -1,6 +1,6 @@
 import { Link, useParams } from "react-router-dom";
 import { ImageContext } from "../context/ImageContext";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import type { Image } from "../types";
 import PageTitle from "../components/PageTitle";
 
@@ -12,8 +12,49 @@ const Photo = () => {
 
     const { images } = useContext(ImageContext);
     const { id } = useParams<keyof RouteParams>() as RouteParams;
+    
+    const defaultCategory = "All Categories";
+    const [filter] = useState<string>(sessionStorage.getItem("category") || defaultCategory);
+    const [currentIndex, setCurrentIndex] = useState(-1);
+    const [prevIndex, setPrevIndex] = useState(-1);
+    const [nextIndex, setNextIndex] = useState(-1);
+
+    const prevRef = useRef<HTMLAnchorElement>(null);
+    const nextRef = useRef<HTMLAnchorElement>(null);
 
     const image = images.find((image: Image) => image.id === id);
+    
+    useEffect(() => {
+        setCurrentIndex(images.findIndex((image: Image) => image.id === id));
+    }, [images, id]);
+
+    useEffect(() => {
+        if (filter === defaultCategory) {
+            setNextIndex(currentIndex + 1 < images.length ? currentIndex + 1 : -1);
+            setPrevIndex(currentIndex - 1);
+        } else {
+            const n = images.slice(currentIndex + 1).findIndex(img => img.category === filter);
+            setNextIndex(n === -1 ? -1 : currentIndex + 1 + n);
+            const p = images.slice(0, currentIndex).reverse().findIndex(img => img.category === filter);
+            setPrevIndex(p === -1 ? -1 : currentIndex - 1 - p);
+        }
+    }, [currentIndex]);
+
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "ArrowLeft") {
+                prevRef.current?.click();
+            }
+            if (e.key === "ArrowRight") {
+                nextRef.current?.click();
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, []);
+    
+
     if (!image) return (
         <h1 className="text-center text-5xl font-semibold bg-c5 text-c1 py-50">
             Sorry! Image Not Found.
@@ -26,11 +67,24 @@ const Photo = () => {
             <Link className="bg-c4 text-content text-white text-xl py-2 px-3 my-5 w-50 text-center rounded-2xl shadow-xl hover:bg-c2 transition" to="/gallery">
                 ← Back to Gallery
             </Link>
-            <img
-                src={`${import.meta.env.BASE_URL}images/${image.filename}`}
-                alt={`Image ${image.id}`}
-                className="max-w-[100vw] md:max-w-[90vw] rounded-lg shadow-xl/30"
-            />
+            <div className="relative w-[90vw]">
+                <img
+                    src={`${import.meta.env.BASE_URL}images/${image.filename}`}
+                    alt={`Image ${image.id}`}
+                    className="w-full h-auto block rounded-lg shadow-xl/30"
+                />
+                {prevIndex >= 0 && 
+                    <Link ref={prevRef} className="absolute left-2 top-1/2 transform -translate-y-1/2 text-white text-3xl font-bold" to={`/photo/${images[prevIndex].id}`}>
+                        ←
+                    </Link>
+                }
+                {nextIndex >= 0 &&
+                    <Link ref={nextRef} className="absolute right-2 top-1/2 transform -translate-y-1/2 text-white text-3xl font-bold" to={`/photo/${images[nextIndex].id}`}>
+                        →
+                    </Link>
+                }
+            </div>
+            
             {/* Description */}
             <p className="font-content text-c5 text-lg mt-4 mx-auto mt-4 px-4 text-wrap text-center">
                 {image.description}
